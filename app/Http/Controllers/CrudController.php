b281cfa9dc93b9c60723aa90c144f83df373b920
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-15 23:21:13 +0300

    Updated controller

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -3,28 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
 use App\Crud;
 
 class CrudController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resources.
      *
-     * @return \Illuminate\Http\Response
+     * @return \App\crud[]\
      */
     public function index()
     {
-        //
+        return Crud::all();
     }
 
     /**
-     * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     *
+     * @deprecated
      */
     public function create()
     {
@@ -35,30 +31,29 @@ class CrudController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \App\Crud
      */
     public function store(Request $request)
     {
-        //
+        $task = Crud::create($request->all());
+        return response()->json($task, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Crud  $crud
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Crud $crud)
     {
-        $crud = Crud::find($id);
-        return view('crud.show', array('crud' => $crud));
+        return Crud::find($crud);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @deprecated
      */
     public function edit($id)
     {
@@ -69,22 +64,27 @@ class CrudController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Crud  $crud
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Crud $crud)
     {
-        //
+        $crud->update($request->all());
+
+        return response()->json($crud, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Crud  $crud
      * @return \Illuminate\Http\Response
+     * @throws \HttpResponseException
      */
-    public function destroy($id)
+    public function delete(Crud $crud)
     {
-        //
+        $crud->delete();
+
+        return response()->json(null, 201);
     }
 }
