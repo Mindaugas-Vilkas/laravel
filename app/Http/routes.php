42fbfc8b7f8d2eaf6bf1a8729a49233aefe9051f
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-15 23:19:57 +0300

    routes extension

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,12 +11,49 @@
 |
 */
 
+Use App\Crud;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('listControl', function() {
-    return view('listControl');
+    return view('listActions');
 });
 
-Route::resource('crud', 'CrudController');
+Route::get('create', function() {
+    return view('create');
+});
+
+
+//-----------------------------------
+
+Route::get('rest/v1', 'CrudController@index');
+Route::get('rest/v1/{id}', 'CrudController@show');
+Route::post('rest/v1', 'CrudController@store');
+Route::put('rest/v1/{id}', 'CrudController@update');
+Route::delete('rest/v1/{id}', 'CrudController@delete');
+
+//-----------------------------------
+
+Route::post('rest/v1', function (Request $request) {
+    return Crud::create($request->all());
+});
+
+Route::get('rest/v1', function () {
+    return Crud::all();
+});
+
+Route::get('rest/v1/{id}', function ($id) {
+    return Crud::find($id);
+});
+
+Route::put('rest/v1/{id}', function ( $id, Request $request) {
+    $crud = Crud::findOrFail($id);
+    $crud->update($request->all());
+    return $crud;
+});
+
+Route::delete('rest/v1/{id}', function ($id) {
+    Crud::find($id)->delete();
+});
