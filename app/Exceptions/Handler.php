9d18bd0055cf31673579a3eba09af9404b40160b
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-15 23:30:05 +0300

    Tweaked handler to return json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,7 +43,9 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
         if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
+            return response()->json([
+                'error' => 'Object could not be found',
+            ], 404);
         }
 
         return parent::render($request, $e);
