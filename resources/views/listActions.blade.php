c62403bb0dde6bccd8b082ad5db20a69294a7720
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-15 23:18:14 +0300

    a few new views

---
+++ resources/views/listActions.blade.php
@@ -0,0 +1,27 @@
+@extends('base')
+
+@section('content')
+    <section>
+        <a href="/create">Create Task</a>
+        <br>
+        <p>--------------------------------------------</p>
+    </section>
+
+    <section>
+        <a href="/somewhereThatJustDisplaysThisShit">Read</a>
+        <br>
+        <p>--------------------------------------------</p>
+    </section>
+
+    <section>
+        <a href="/update">Update Task</a>
+        <br>
+        <p>--------------------------------------------</p>
+    </section>
+
+    <section>
+        <a href="/delete">Delete Task</a>
+        <br>
+        <p>--------------------------------------------</p>
+    </section>
+@stop
