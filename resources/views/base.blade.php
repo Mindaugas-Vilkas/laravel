88d30c8b89ecba1b2172ce3c4c1bcb820941123c
Mindaugas Vilkas <Quitarias@gmail.com>
2018-08-15 18:42:33 +0300

    Initial commit, with database, route and basic views

---
+++ resources/views/base.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <h1>Laravel task 1</h1>
+
+        @yield('content')
+    </body>
+</html>
