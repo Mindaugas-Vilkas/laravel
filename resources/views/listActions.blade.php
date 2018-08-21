792cd5e6ea33880d7a915512f166df2729db7044
Mindaugas Vilkas <mindaugasVilkaas@gmail.com>
2018-08-21 21:42:34 +0300

    changed the view over from the interactive to just a documentation page

--- resources/views/listActions.blade.php
+++ resources/views/listActions.blade.php
@@ -2,25 +2,32 @@
 
 @section('content')
     <section>
-        <a href="/create">Create Task</a>
+        <h3>Create Task</h3>
+        <p>Send a POST request to /rest/v1</p>
+        <p>Body should contain json formated as {"taskName":"{$taskName}", "$taskDescription":"{$taskDescription}", "dueDate":"{$date(format(YYYY-MM-DD))}|"}</p>
         <br>
         <p>--------------------------------------------</p>
     </section>
 
     <section>
-        <a href="/somewhereThatJustDisplaysThisShit">Read</a>
+        <h3>Read</h3>
+        <p>Send a GET request to /rest/v1</p>
+        <p>No other parameters required.</p>
         <br>
         <p>--------------------------------------------</p>
     </section>
 
     <section>
-        <a href="/update">Update Task</a>
+        <h3>Update Task</h3>
+        <p>Send a PUT request to /rest/v1/{$id}</p>
+        <p>Body should contain json formated as {"taskName":"{$taskName}", "$taskDescription":"{$taskDescription}", "dueDate":"{$date(format(YYYY-MM-DD))}|"}</p>
         <br>
         <p>--------------------------------------------</p>
     </section>
 
     <section>
-        <a href="/delete">Delete Task</a>
+        <h3>Delete Task</h3>
+        <p>Send a DELETE request to /rest/v1/{$id}</p>
         <br>
         <p>--------------------------------------------</p>
     </section>
